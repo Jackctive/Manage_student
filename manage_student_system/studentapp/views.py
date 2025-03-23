c8defe766ce6e1e77942cb6f7ff5d1df23b2a193
Jackctive <chanhungdo1@gmail.com>
2025-03-23 11:21:13 +0700

    get data info, create object sinh_vien

--- manage_student_system/studentapp/views.py
+++ manage_student_system/studentapp/views.py
@@ -8,4 +8,16 @@ def danh_sach_sinh_vien(request):
     return render(request, 'students/index.html', context)
 
 def add_student(request):
+    if request.method == 'POST':
+        # Lấy dữ liệu từ form
+        mssv = request.POST.get('mssv')
+        ho_ten = request.POST.get('ho_ten')
+        ngay_sinh = request.POST.get('ngay_sinh')
+        email = request.POST.get('email')
+        so_dien_thoai = request.POST.get('so_dien_thoai')
+        lop = request.POST.get('lop')
+        nganh_hoc = request.POST.get('nganh_hoc')  
+
+        new_student = SinhVien(mssv=mssv, ho_ten=ho_ten, ngay_sinh=ngay_sinh, email=email, so_dien_thoai=so_dien_thoai, lop=lop, nganh_hoc=nganh_hoc)
+
     return render(request, 'students/add_student.html')
