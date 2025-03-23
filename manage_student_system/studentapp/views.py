3b1b40e2ec1cf3f228c02a708e29faf8e44e69ca
Jackctive <chanhungdo1@gmail.com>
2025-03-23 11:38:21 +0700

    update message box, after saving object in database

--- manage_student_system/studentapp/views.py
+++ manage_student_system/studentapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import SinhVien
+from django.shortcuts import redirect
+from django.contrib import messages
 
 def danh_sach_sinh_vien(request):
     sinhviens = SinhVien.objects.all()  # Lấy tất cả sinh viên từ database
@@ -20,4 +22,17 @@ def add_student(request):
 
         new_student = SinhVien(mssv=mssv, ho_ten=ho_ten, ngay_sinh=ngay_sinh, email=email, so_dien_thoai=so_dien_thoai, lop=lop, nganh_hoc=nganh_hoc)
 
+        # Ceate new student and save to database
+        try:
+            # Save the new student to the database
+            new_student.save()
+            
+            messages.success(request, "Sinh viên đã được thêm thành công!")
+            print("Sinh viên đã được thêm thành công!")
+
+            return redirect('ds_sv')  # Redirect to the student list page
+
+        except Exception as e:
+            messages.error(request, f"Đã xảy ra lỗi: {e}")
+            print(f"Đã xảy ra lỗi: {e}")
     return render(request, 'students/add_student.html')
